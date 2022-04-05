d9afc98817bf0c4d85dda7140857e17df1a2ff6d
DPascuttini <dominik.pascuttini1@gmail.com>
2022-04-05 21:13:39 +0200

    Imported new features into the main app file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { SearchBar } from './components/layout/SearchBar';
 import { Logs } from './components/layout/logs/Logs';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import { AddBtn } from './components/layout/AddBtn';
 import './App.css';
+import { AddLogModal } from './components/layout/logs/AddLogModal';
 
 const App = () => {
   useEffect(() => {
@@ -14,6 +16,8 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
